6d4a47feb787ccef210ac2e2b47b58507e496a1d
ErAbhi90 <er.abhithakral@gmail.com>
2019-05-25 00:51:40 +0530

    Code Refactor and app completed

--- lib/ui/story/story_page.dart
+++ lib/ui/story/story_page.dart
@@ -57,8 +57,6 @@ class _StoryPageState extends State<StoryPage> {
             ),
             Expanded(
               flex: 2,
-
-              //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
               child: Visibility(
                 visible: _storyBrain.buttonShouldBeVisible(),
                 child: FlatButton(
@@ -84,5 +82,3 @@ class _StoryPageState extends State<StoryPage> {
     );
   }
 }
-
-//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
